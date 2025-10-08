4b847057ef9cc9154fe0b20a5fdf233daecc1a67
Ping Hong Chew <pinghong.chew@semi-tech.com>
2025-10-08 13:53:37 +0800

    add base: sts-toolkit and page icon

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: '/sts-toolkit/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
